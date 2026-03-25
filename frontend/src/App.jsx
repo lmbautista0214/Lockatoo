8751e9242dd2d904fadc4dbc7c3985191d6eb595
lloyd michael bautista <lloydmichaelbautista1995@gmail.com>
2026-03-25 12:23:08 +0000

    Feat/api/auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
 import MockPayment from "./components/MockPayment";
 
 function App() {
-
   return (
     <>
+      <Register />
+      <Login />
       <MockPayment />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
